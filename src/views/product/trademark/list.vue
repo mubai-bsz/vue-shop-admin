7d27b9e2d18c59cddcb4dc5129432df8dda38d12
mubai <2817368183@qqcom>
2020-12-11 13:00:38 +0800

    feat:品牌管理页面搭建

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,11 +1,55 @@
 <template>
-<div>
-  品牌管理
-</div>
+  <div>
+    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <el-table :data="tableData" border style="width: 100%; margin: 20px 0">
+      <el-table-column prop="num" label="序号" width="80"> </el-table-column>
+      <el-table-column prop="name" label="品牌名称"> </el-table-column>
+      <el-table-column label="品牌LOGO">
+        <template slot-scope="scope">
+          <img class="trademark-img" :src="scope.row.logo" alt="logo" />
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="操作">
+        <template>
+          <el-button type="warning" icon="el-icon-edit">修改</el-button>
+          <el-button type="danger" icon="el-icon-delete">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页器 -->
+    <el-pagination
+      class="trademark-pagination"
+      :page-sizes="[3, 6, 9]"
+      :page-size="5"
+      layout="prev, pager, next, jumper, sizes, total"
+      :total="50"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TrademarkList',
-}
+  name: "TrademarkList",
+  data() {
+    return {
+      tableData: [
+        {
+          num: "1",
+          name: "huawei",
+          logo:
+            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3638032789,1833523001&fm=26&gp=0.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
+<style lang="sass">
+.trademark-img
+  width: 150px
+.trademark-pagination
+  text-align: right
+.el-pagination__sizes
+  margin-left: 450px
+</style>
